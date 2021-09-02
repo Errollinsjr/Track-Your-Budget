348fdb1f24305ea26944402826a8eee49c9d2ad0
Errollinsjr <errollins.jr@gmail.com>
2021-09-02 11:52:57 -0400

    working on deleting old cache data

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,42 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
+
+//add event handler
+self.addEventListener("install", function(event) {
+    //cached transaction data
+    event.waitUntil(
+        caches.open(DATA_CACHE_NAME)
+        .then((cache) => 
+        cache.add("/api/transaction"))
+    );
+    
+    //prestored things in cache ready to load
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => 
+        cache.addAll(FILES_TO_CACHE))
+    );
+
+    //tells serviceworker to kick off immediately after everything loads
+    self.skipWaiting();
+});
+
+//activate service-worker
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then(keyList => {
+            // return all async promises and map through keyList to delete any old cache data via key
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Deleted old cache'd data", key);
+                        return caches.delete(key);
+                    };
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
